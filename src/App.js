95f8163f893af3392bac0f7dc08347da9584cb0b
John David Tchomgui <tchomguijohn@gmail.com>
2025-01-15 02:41:04 +0100

    expect for last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Home/Navbar';
 import Home from './Pages/Home/Homescreen';
 
-
 function App() {
   return (
-  <div className="App">
-   <Router>
-    <div>
-      <Navbar/>
+    <div className="App">
+      <Navbar />
       <Routes>
-        <Route path= "/" element={<Home/>}>
-        </Route>
-        <Route path="*" element={<div>404 not Found</div>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </div>
-   </Router>
-  </div>
   );
 }
 
